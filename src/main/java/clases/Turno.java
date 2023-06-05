24ec7734ce21ed441862d27b2dcd8405aff1d56b
sebastianolaran <solaran@fi.uba.ar>
2023-06-05 11:29:41 -0300

    Agrego test que verifica funcionamiento de lectura json e interfaz Turno

---
+++ src/main/java/clases/Turno.java
@@ -0,0 +1,6 @@
+package clases;
+
+public interface Turno {
+
+    void ejecutarTurno();
+}
---
+++ src/main/java/clases/TurnoIA.java
@@ -0,0 +1,71 @@
+package clases;
+
+import org.json.simple.parser.ParseException;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class TurnoIA implements Turno {
+
+    Map<Integer, List<String>> enemigosTurno;
+
+    int turnoActual;
+
+    Mapa mapa;
+
+    private List<Enemigo> enemigos;
+
+
+    public TurnoIA(Mapa mapa) throws IOException, ParseException {
+        ConvertidorEnemigos convertidor = new ConvertidorEnemigosImplementacion();
+        this.enemigosTurno = convertidor.cargarEnemigos("src/temp/enemigos.json");
+        this.turnoActual = 1;
+        this.enemigos = new ArrayList<>();
+        this.mapa = mapa;
+    }
+
+    public void ejecutarTurno() {
+        this.cargarEnemigos();
+        this.moverEnemigos();
+        if (this.turnoActual < 12) {
+            this.turnoActual++;
+        } else {
+            this.turnoActual = 1;
+        }
+    }
+
+
+    public Enemigo definirEnemigo(String nombre) {
+        Enemigo enemigo = null;
+        switch (nombre) {
+            case "arana":
+                enemigo = new Arania(mapa.getOrigen());
+                break;
+            case "hormiga":
+                enemigo = new Hormiga(mapa.getOrigen());
+                break;
+            default:
+                //Excepcion
+                break;
+        }
+        return enemigo;
+
+    }
+
+    public void cargarEnemigos() {
+        for (String valor : enemigosTurno.get(turnoActual)) {
+            Enemigo enemigo = this.definirEnemigo(valor);
+            enemigos.add(enemigo);
+        }
+
+    }
+
+    public void moverEnemigos() {
+        for (Enemigo enemigo : enemigos) {
+            enemigo.mover(this.mapa);
+        }
+    }
+
+}
