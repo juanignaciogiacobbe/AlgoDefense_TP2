bbcac6992f7a7b1f7343102061aaa5fe508a5c1b
Luis Cusihuaman <luiscusihuaman88@gmail.com>
2023-06-06 03:55:37 -0300

    chore: use helper instead of mockito

--- src/main/test/ConvertidoresTest.java
+++ src/main/test/ConvertidoresTest.java
@@ -3,10 +3,10 @@ import clases.ConvertidorEnemigosImplementacion;
 import clases.Enemigo;
 import org.json.simple.parser.ParseException;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
 
-import java.io.FileNotFoundException;
+import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.List;
 import java.util.Map;
@@ -17,57 +17,49 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class ConvertidoresTest {
 
+
 	@Test
-	void ConvertidorEnemigosEsCorrecto() throws FileNotFoundException {
-		FileReader fileReader = new FileReader("src/temp/enemigos.json");
+	void ConvertidorEnemigosEsCorrecto() {
+		String mockedJson = "[{\"turno\": 4, \"enemigos\": {\"hormiga\": 0, \"arana\": 1}}]";
+		FileReader fileReader = createFileReaderWithContent(mockedJson);
+
 		ConvertidorEnemigos convertidor = new ConvertidorEnemigosImplementacion(fileReader);
 		AtomicReference<Map<Integer, List<Enemigo>>> enemigosPorRonda = new AtomicReference<>();
 		assertDoesNotThrow(() -> enemigosPorRonda.set(convertidor.cargarEnemigos()));
 		assertNotNull(enemigosPorRonda);
 
-		// Comprobar las rondas 4 y 5
 		int rondaAComprobar = 4;
 		assertTrue(enemigosPorRonda.get().containsKey(rondaAComprobar), "Falta la ronda " + rondaAComprobar);
 		List<Enemigo> enemigosRonda4 = enemigosPorRonda.get().get(rondaAComprobar);
 		assertNotNull(enemigosRonda4, "La lista de enemigos para la ronda " + rondaAComprobar + " es nula");
 		assertEquals(1, enemigosRonda4.size(), "Número incorrecto de enemigos para la ronda " + rondaAComprobar);
 
-		rondaAComprobar = 5;
-		assertTrue(enemigosPorRonda.get().containsKey(rondaAComprobar), "Falta la ronda " + rondaAComprobar);
-		List<Enemigo> enemigosRonda5 = enemigosPorRonda.get().get(rondaAComprobar);
-		assertNotNull(enemigosRonda5, "La lista de enemigos para la ronda " + rondaAComprobar + " es nula");
-		assertEquals(2, enemigosRonda5.size(), "Número incorrecto de enemigos para la ronda " + rondaAComprobar);
-
-		// Comprobar la longitud de las rondas
-		int rondasEsperadas = 12;
+		int rondasEsperadas = 1;
 		assertEquals(rondasEsperadas, enemigosPorRonda.get().size(), "Número incorrecto de rondas");
 	}
 
 	@Test
-	void cargarEnemigos_ParseException() {
+	void cargarEnemigos_InvalidJson() {
 		// Arrange
-		FileReader fileReaderMock = Mockito.mock(FileReader.class);
+		String invalidJson = "{";
+		FileReader fileReaderMock = createFileReaderWithContent(invalidJson);
 		ConvertidorEnemigosImplementacion convertidor = new ConvertidorEnemigosImplementacion(fileReaderMock);
 
 		// Act & Assert
 		assertThrows(ParseException.class, convertidor::cargarEnemigos);
 	}
-	@Test
-	void cargarEnemigos_InvalidJson() throws IOException {
-		// Arrange
-		String invalidJson = "invalid json";
-		FileReader fileReaderMock = Mockito.mock(FileReader.class);
-		ConvertidorEnemigosImplementacion convertidor = new ConvertidorEnemigosImplementacion(fileReaderMock);
 
-		// Mock FileReader behavior
-		Mockito.when(fileReaderMock.read(Mockito.any(char[].class))).thenAnswer(invocation -> {
-			char[] buffer = invocation.getArgument(0);
-			System.arraycopy(invalidJson.toCharArray(), 0, buffer, 0, invalidJson.length());
-			return invalidJson.length();
-		});
-
-		// Act & Assert
-		assertThrows(ParseException.class, convertidor::cargarEnemigos);
+	private FileReader createFileReaderWithContent(String content) {
+		File inputFile;
+		try {
+			inputFile = File.createTempFile("test_input", ".json");
+			inputFile.deleteOnExit();
+			FileWriter fileWriter = new FileWriter(inputFile);
+			fileWriter.write(content);
+			fileWriter.close();
+			return new FileReader(inputFile);
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
 	}
-
 }
