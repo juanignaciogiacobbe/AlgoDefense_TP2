132a2b18893131f74193b8581523ebb35fa13ec8
sebastianolaran <solaran@fi.uba.ar>
2023-06-08 01:18:00 -0300

    test 18 y 19 funcionando

--- src/main/java/clases/AlgoDefense.java
+++ src/main/java/clases/AlgoDefense.java
@@ -16,86 +16,106 @@ import java.util.Map;
 
 public class AlgoDefense {
 
-	private Jugador jugador1;
+    private Jugador jugador1;
 
-	private Mapa mapa;
+    private Mapa mapa;
 
-	private List<Enemigo> enemigos;
+    private List<Enemigo> enemigos;
 
-	public AlgoDefense() throws IOException, ParseException, FormatoJSONInvalidoException, NombreInvalido {
-		FileReader reader = new FileReader("src/temp/mapa.json");
+    private List<ParcelaDeTierra> defensas;
+
+    public AlgoDefense() throws IOException, ParseException, FormatoJSONInvalidoException, NombreInvalido {
+        FileReader reader = new FileReader("src/temp/mapa.json");
         ConvertidorMapa convertidor = new ConvertidorMapaImplementacion(reader);
-		this.mapa = convertidor.cargarMapa();
-		this.enemigos = new ArrayList<>();
-	}
+        this.mapa = convertidor.cargarMapa();
+        this.enemigos = new ArrayList<>();
+        this.defensas = new ArrayList<>();
+    }
 
-	public Mapa getMapa() {
-		return this.mapa;
-	}
+    public Mapa getMapa() {
+        return this.mapa;
+    }
 
-	public void agregarJugador(String nombre) throws NombreInvalido {
-		if (nombre.length() < 6) throw new NombreInvalido();
+    public void agregarJugador(String nombre) throws NombreInvalido {
+        if (nombre.length() < 6) throw new NombreInvalido();
 
-		this.jugador1 = new Jugador(nombre);
-	}
-
-
-	public String finDelJuego() {
-
-		if (jugador1.getVida() <= 0 || enemigos.isEmpty()){
-			return "Computadora";
-		}
-		return jugador1.getNombre();
-
-	}
-
-	public void moverEnemigos() throws TerrenoNoAptoParaConstruir, TerrenoNoAptoParaCaminar, SinVidaRestante {
-		for (Enemigo enemigo : enemigos) {
-			enemigo.mover(this.mapa);
-			}
-
-		this.enemigos = this.mapa.getMeta().actualizarEnemigos(this.enemigos,jugador1);
-
-	}
+        this.jugador1 = new Jugador(nombre);
+    }
 
 
+    public String finDelJuego() {
 
-	public void agregarEnemigo(Enemigo enemigo) {
-		enemigos.add(enemigo);
-	}
+        if (jugador1.getVida() <= 0 || enemigos.isEmpty()) {
+            return "Computadora";
+        }
+        return jugador1.getNombre();
 
-	public int enemigosEnMeta() {
-		int cantidadMeta = 0;
-		for (Enemigo enemigo : enemigos) {
-			if (enemigo.getPasarelaActual().getCoordenada().equals(mapa.getMeta().getCoordenada())) {
-				cantidadMeta += 1;
-			}
-		}
-		return cantidadMeta;
-	}
+    }
 
-	public void comenzaPartida() throws IOException, ParseException {
-		//TurnoJugador turnoJugador = new TurnoJugador();
-		//TurnoIA turnoIA = new TurnoIA();
-		//while (partida sea jugable)
-		//turnoJugador.ejecutarTurno();
-		//turnoIA.ejecutarTurno();
+    public void moverEnemigos() throws TerrenoNoAptoParaConstruir, TerrenoNoAptoParaCaminar, SinVidaRestante {
+        for (Enemigo enemigo : enemigos) {
+            enemigo.mover(this.mapa);
+        }
 
-	}
+        this.enemigos = this.mapa.getMeta().actualizarEnemigos(this.enemigos, jugador1);
+
+    }
 
 
-	public void cargarEnemigos () throws FileNotFoundException, FormatoJSONInvalidoException, ParseException {
-		FileReader readerEnemigos = new FileReader("src/temp/enemigos.json");
-		ConvertidorEnemigos convertidorEnemigos = new ConvertidorEnemigosImplementacion(readerEnemigos);
-		Map<Integer, List<Enemigo>> enemigosPorRonda = convertidorEnemigos.cargarEnemigos();
-		for (int i = 1 ; i < 12 ; i++) {
-			for (Enemigo enemigoDefinido : enemigosPorRonda.get(i)) {
-				enemigoDefinido.setPasarelaActual(mapa.getOrigen());
-				enemigos.add(enemigoDefinido);
-			}
-		}
+    public void agregarEnemigo(Enemigo enemigo) {
+        enemigos.add(enemigo);
+    }
 
-	}
+    public int enemigosEnMeta() {
+        int cantidadMeta = 0;
+        for (Enemigo enemigo : enemigos) {
+            if (enemigo.getPasarelaActual().getCoordenada().equals(mapa.getMeta().getCoordenada())) {
+                cantidadMeta += 1;
+            }
+        }
+        return cantidadMeta;
+    }
+
+    public void comenzaPartida() throws IOException, ParseException {
+        //TurnoJugador turnoJugador = new TurnoJugador();
+        //TurnoIA turnoIA = new TurnoIA();
+        //while (partida sea jugable)
+        //turnoJugador.ejecutarTurno();
+        //turnoIA.ejecutarTurno();
+
+    }
 
 
+    public void cargarEnemigos(int cantTurnos) throws FileNotFoundException, FormatoJSONInvalidoException, ParseException {
+        FileReader readerEnemigos = new FileReader("src/temp/enemigos.json");
+        ConvertidorEnemigos convertidorEnemigos = new ConvertidorEnemigosImplementacion(readerEnemigos);
+        Map<Integer, List<Enemigo>> enemigosPorRonda = convertidorEnemigos.cargarEnemigos();
+        for (int i = 1; i < cantTurnos; i++) {
+            for (Enemigo enemigoDefinido : enemigosPorRonda.get(i)) {
+                enemigoDefinido.setPasarelaActual(mapa.getOrigen());
+                enemigos.add(enemigoDefinido);
+            }
+        }
+
+    }
+
+    public void ubicarDefensa(Defensa defensa, int absica, int ordenada) {
+        ParcelaDeTierra parcelaSet = null;
+        Coordenada coordenadaset = new Coordenada(absica, ordenada);
+        for (Parcela parcela : mapa.getParcelas()) {
+            if (parcela.getCoordenada().equals(coordenadaset)) {
+                parcelaSet = (ParcelaDeTierra) parcela;
+            }
+        }
+        parcelaSet.setDefensa(defensa);
+        defensas.add(parcelaSet);
+    }
+
+
+    public void activarDefensas() {
+        for (ParcelaDeTierra parcela : defensas) {
+            parcela.getDefensa().atacar(enemigos, parcela);
+        }
+
+    }
 }
