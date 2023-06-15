49c57a179e08d527a5959c12945a2b8ab6e406b2
Luis Cusihuaman <luiscusihuaman88@gmail.com>
2023-06-14 22:39:17 -0300

    feat(defensa): trampa arenosa terrenos validos
    Co-authored-by: valeriafb <valeriafboch@gmail.com>

---
+++ src/main/java/edu/fiuba/algo3/modelo/parcelas/TrampaArenosa.java
@@ -0,0 +1,7 @@
+package edu.fiuba.algo3.modelo.parcelas;
+
+import edu.fiuba.algo3.modelo.defensas.Defensa;
+
+public class TrampaArenosa extends Defensa {
+
+}
--- src/main/test/edu/fiuba/algo3/DefensaTest.java
+++ src/main/test/edu/fiuba/algo3/DefensaTest.java
@@ -2,9 +2,16 @@ package edu.fiuba.algo3;
 
 import edu.fiuba.algo3.modelo.defensas.TorreBlanca;
 import edu.fiuba.algo3.modelo.defensas.TorrePlateada;
+import edu.fiuba.algo3.modelo.juego.Jugador;
+import edu.fiuba.algo3.modelo.juego.NombreInvalido;
+import edu.fiuba.algo3.modelo.parcelas.PasarelaLargada;
+import edu.fiuba.algo3.modelo.parcelas.PasarelaMeta;
+import edu.fiuba.algo3.modelo.parcelas.TerrenoNoAptoParaConstruir;
+import edu.fiuba.algo3.modelo.parcelas.TrampaArenosa;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 
 public class DefensaTest {
@@ -24,15 +31,13 @@ public class DefensaTest {
 		assertEquals(torrePlateada.getCostoConstruccion(), creditosNecesariosParaConstruirLaDefensa);
 	}
 
-    /*@Test
-
-    public void test03PruebaFuncionamiento() {
-        AlgoDefense algo = new AlgoDefense();
-        Hormiga hormiga = new Hormiga(algo.getMapa().getOrigen());
-        algo.agregarEnemigo(hormiga);
-        algo.moverEnemigos();
-        algo.moverEnemigos();
-        algo.comenzarAtaqueJugador();
-        assertEquals(0,hormiga.getVida());
-    }*/
+	@Test
+	public void test03UnaTrampaArenosaNoSePuedeConstruirEnUnPasarelaInvalida() throws NombreInvalido {
+		Jugador jugador = new Jugador("Jugador");
+		TrampaArenosa trampaArenosa = new TrampaArenosa();
+		PasarelaLargada pasarelaLargada = new PasarelaLargada(0, 1);
+		PasarelaMeta pasarelaMeta = new PasarelaMeta(0, 2);
+		assertThrows(TerrenoNoAptoParaConstruir.class, () -> jugador.construir(trampaArenosa, pasarelaLargada));
+		assertThrows(TerrenoNoAptoParaConstruir.class, () -> jugador.construir(trampaArenosa, pasarelaMeta));
+	}
 }
