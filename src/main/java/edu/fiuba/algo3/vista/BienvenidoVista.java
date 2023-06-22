2155bbeb622799823cb0ee35bbd44e0b06ec8021
Luis Cusihuaman <luiscusihuaman88@gmail.com>
2023-06-22 00:01:16 -0300

    feat(ui): logo

--- src/main/java/edu/fiuba/algo3/vista/BienvenidoVista.java
+++ src/main/java/edu/fiuba/algo3/vista/BienvenidoVista.java
@@ -1,9 +1,13 @@
 package edu.fiuba.algo3.vista;
 
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 
 public class BienvenidoVista implements Vista {
@@ -21,9 +25,9 @@ public class BienvenidoVista implements Vista {
 			nameField.getStyleClass().add("input-invalido");
 			return;
 		}
-			System.out.println(playerName + ", bienvenido a AlgoDefense");
-			if (nextVista != null) {
-				nextVista.mostrar(scene);
+		System.out.println(playerName + ", bienvenido a AlgoDefense");
+		if (nextVista != null) {
+			nextVista.mostrar(scene);
 		}
 	}
 
@@ -41,7 +45,25 @@ public class BienvenidoVista implements Vista {
 
 		VBox vbox = new VBox(nameLabel, nameField, loginButton);
 		vbox.getStyleClass().add("container");
+
+		// Create a StackPane to hold the logo image and input field
+		StackPane stackPane = new StackPane();
+		stackPane.getChildren().addAll(
+				createLogoImageView(), // Add logo image view
+				vbox
+		);
+		StackPane.setMargin(vbox, new Insets(250, 0, 0, 0)); // Adjust margin to position the input field
+
 		loginButton.setOnAction(e -> validateAndHandleInput(scene));
-		scene.setRoot(vbox);
+		scene.setRoot(stackPane);
+	}
+
+	private ImageView createLogoImageView() {
+		String imagePath = "file:src/resources/logo.png";
+		Image image = new Image(imagePath);
+		ImageView imageView = new ImageView(image);
+		imageView.setPreserveRatio(true);
+		imageView.setFitWidth(500);
+		return imageView;
 	}
 }
